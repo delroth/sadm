e4fee289576b5550d6bd199ae60567f8595f9df6
Alexandre Macabies <web+oss@zopieux.com>
2018-11-10 19:43:24 +0100

    buildbot: make_manifest: port to Python 3 and cleanup

--- buildbot/make_manifest.py
+++ buildbot/make_manifest.py
@@ -1,5 +1,6 @@
-#! /usr/bin/env python2
+#!/usr/bin/env python3
 
+from pathlib import Path
 import argparse
 import base64
 import gzip
@@ -7,17 +8,15 @@ import hashlib
 import libarchive.public
 import nacl.encoding
 import nacl.signing
-import os
-import os.path
 import sys
 
 
-def write_to_content_store(base, h, contents):
-    directory = os.path.join(base, h[0:2], h[2:4])
-    if not os.path.isdir(directory):
-        os.makedirs(directory)
-    path = os.path.join(directory, h[4:])
-    if os.path.exists(path):
+def write_to_content_store(base: Path, h: str, contents: bytes):
+    directory = base / h[0:2] / h[2:4]
+    directory.mkdir(parents=True, exist_ok=True)
+    path = directory / h[4:]
+    # Path is content-based, so no need to overwrite.
+    if path.exists():
         return
     with gzip.GzipFile(path, "wb") as fp:
         fp.write(contents)
@@ -27,62 +26,64 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Generate an update manifest file.')
     parser.add_argument(
-        '--input', required=True, help='Input archive to process.')
+        '--input', required=True, type=Path, help='Input archive to process.')
     parser.add_argument(
         '--version_hash',
         required=True,
         help='SHA1 Git hash of the version being stored.')
     parser.add_argument(
         '--output-manifest-store',
+        type=Path,
         help='If provided, write the manifest to the store at this path.')
     parser.add_argument(
         '--output-content-store',
+        type=Path,
         help='If provided, write the content to the store at this path.')
     parser.add_argument(
-        '--signing-key', required=True, help='Ed25519 signing key.')
+        '--signing-key',
+        required=True,
+        type=argparse.FileType(mode='rb'),
+        help='Ed25519 signing key.')
     args = parser.parse_args()
 
     entries = []
-    with libarchive.public.file_reader(args.input) as archive:
+    with libarchive.public.file_reader(str(args.input)) as archive:
         for entry in archive:
-            filename = str(entry)
+            filename = entry.pathname
             # Skip directories.
             if filename.endswith('/'):
                 continue
             # Remove the initial directory name.
             filename = filename.split('/', 1)[1]
-            assert "\t" not in filename, "Unsupported char in filename: \\t"
-            contents = ''
-            for block in entry.get_blocks():
-                contents += block
+            if "\t" in filename:
+                raise ValueError(r"Unsupported char in filename: \t")
+            contents = b''.join(entry.get_blocks())
             h = hashlib.sha256(contents).hexdigest()[:32]
             if args.output_content_store:
                 write_to_content_store(args.output_content_store, h, contents)
             entries.append((filename, h))
 
     entries.sort()
-    manifest = "".join("%s\t%s\n" % e for e in entries)
+    manifest = ("".join(
+        f"{filename}\t{h}\n" for filename, h in entries).encode('utf-8'))
 
     signing_key = nacl.signing.SigningKey(
-        open(args.signing_key).read(), encoder=nacl.encoding.RawEncoder)
+        args.signing_key.read(), encoder=nacl.encoding.RawEncoder)
     sig = base64.b64encode(signing_key.sign(manifest).signature)
 
     if args.output_manifest_store:
-        directory = os.path.join(args.output_manifest_store,
-                                 args.version_hash[0:2],
-                                 args.version_hash[2:4])
-        filename = args.version_hash[4:] + ".manifest"
-        if not os.path.isdir(directory):
-            os.makedirs(directory)
-        fp = gzip.GzipFile(os.path.join(directory, filename + ".tmp"), "wb")
+        directory = (args.output_manifest_store / args.version_hash[0:2] /
+                     args.version_hash[2:4])
+        directory.mkdir(parents=True, exist_ok=True)
+        filename = directory / (args.version_hash[4:] + ".manifest")
+        temp_filename = filename.with_suffix(".manifest.tmp")
+        fp = gzip.GzipFile(temp_filename, "wb")
     else:
-        fp = sys.stdout
+        fp = sys.stdout.buffer
 
     fp.write(manifest)
-    fp.write("\n" + sig + "\n")
+    fp.write(b"\n" + sig + b"\n")
 
     if args.output_manifest_store:
         fp.close()
-        os.rename(
-            os.path.join(directory, filename + ".tmp"),
-            os.path.join(directory, filename))
+        temp_filename.rename(filename)
