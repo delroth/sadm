93fa7f2717c5c4ffe33a86d7a59ed45370891805
Alexandre Macabies <web+oss@zopieux.com>
2018-11-10 18:16:32 +0100

    buildbot: send_build: make all settings overridable through env

--- buildbot/send_build.py
+++ buildbot/send_build.py
@@ -5,18 +5,20 @@ import hmac
 import os
 import requests
 
-CALLBACK_URL = 'https://dolphin-emu.org/download/new/'
-DOWNLOADS_CREATE_KEY = open('/etc/dolphin-keys/downloads-create').read().strip()
-
 
 def get_env_var(name):
     try:
-        return os.environ[name].decode("utf-8")
+        return os.environ[name]
     except KeyError:
         raise KeyError(f"{name} is missing from the environment") from None
 
 
-if __name__ == '__main__':
+def main():
+    key_filename = os.environ.get('DOWNLOADS_CREATE_KEY',
+                                  '/etc/dolphin-keys/downloads-create')
+    callback_url = os.environ.get('CALLBACK_URL',
+                                  'https://dolphin-emu.org/download/new/')
+
     branch = get_env_var('BRANCH')
     shortrev = get_env_var('SHORTREV')
     hash = get_env_var('HASH')
@@ -26,11 +28,14 @@ if __name__ == '__main__':
     build_url = get_env_var('BUILD_URL')
     user_os_matcher = get_env_var('USER_OS_MATCHER')
 
+    with open(key_filename, 'rb') as f:
+        key = f.read().strip()
+
     parts = [branch, shortrev, hash, author, description, target_system,
              build_url, user_os_matcher]
 
     msg = "|".join([str(len(part)) for part in parts] + parts)
-    hm = hmac.new(DOWNLOADS_CREATE_KEY, msg.encode("utf-8"), hashlib.sha1)
+    hm = hmac.new(key, msg.encode("utf-8"), hashlib.sha1)
 
     post_data = {
         'branch': branch,
@@ -44,6 +49,10 @@ if __name__ == '__main__':
         'hmac': hm.hexdigest(),
     }
 
-    r = requests.post(CALLBACK_URL, data=post_data)
+    r = requests.post(callback_url, data=post_data)
     r.raise_for_status()
     print(r.text)
+
+
+if __name__ == '__main__':
+    main()
